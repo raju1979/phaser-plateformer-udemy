62ffbe88f9cd46782187f43c65f3083453ab6d8f
Rajesh D <dhrajesh1@gmail.com>
2020-04-17 08:49:57 +0530

    platform added

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+/// <reference path="../typings/phaser.d.ts" />
 import Phaser from 'phaser';
 
 export default {
@@ -5,9 +6,23 @@ export default {
   parent: 'game',
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 320,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
+  render: {
+    pixelArt: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 750},
+      debug: false,
+      debugShowVelocity: true,
+      debugShowBody: true,
+      debugShowStaticBody: true
+    }
+
+  }
 };
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,18 +1,137 @@
+/// <reference path="../../typings/phaser.d.ts" />
 import Phaser from 'phaser';
+import Hero from '../entities/Hero';
 
 class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'GameScene' });
   }
 
-  init(data) {}
-
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.tilemapTiledJSON('level-1', 'assets/tilemaps/level-1.json');
+
+    this.load.image('world-1-sheet', 'assets/tilesets/world-1.png');
+    this.load.image('clouds-sheet', 'assets/tilesets/Clouds.png');
+
+    this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-pivot-sheet', 'assets/hero/pivot.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-jump-sheet', 'assets/hero/jump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-flip-sheet', 'assets/hero/spinjump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    
+    this.anims.create({
+      key: 'hero-idle',
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'hero-pivoting',
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-jumping',
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'hero-flipping',
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      frameRate: 30,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'hero-falling',
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.addMap();
+
+    this.addHero();
+
+    this.cameras.main.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels);
+    this.cameras.main.startFollow(this.hero);
+
+  }
+
+  addHero() {
+    this.hero = new Hero(this, this.spawnPos.x, this.spawnPos.y);
+    this.children.moveTo(this.hero, this.children.getIndex(this.map.getLayer('Foreground').tilemapLayer))
+
+    this.physics.add.collider(this.hero, this.map.getLayer('Ground').tilemapLayer);
+  }
+
+  addMap() {
+
+    this.map = this.make.tilemap({ key: 'level-1' });
+    const groundTiles = this.map.addTilesetImage('world-1', 'world-1-sheet');
+
+    const backgroundTiles = this.map.addTilesetImage('clouds', 'clouds-sheet');
+
+    this.backgroundLayer = this.map.createStaticLayer('Background', backgroundTiles);
+    this.backgroundLayer.setScrollFactor(0.6);;
+    
+
+    const groundLayer = this.map.createStaticLayer('Ground', groundTiles);
+    groundLayer.setCollision([1,2,4], true);
+
+    this.map.createStaticLayer('Foreground', groundTiles);
+
+    this.physics.world.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels);
+    this.physics.world.setBoundsCollision(true, true, false, true);
+
+    this.map.getObjectLayer('Objects').objects.forEach((object) => {
+      if(object.name == 'Start') {
+        this.spawnPos = {x: object.x, y: object.y};
+      }
+    })
+
+    const debugGraphics = this.add.graphics();
+    // groundLayer.renderDebug(debugGraphics)
+    
+
+
   }
 
   update(time, delta) {}
